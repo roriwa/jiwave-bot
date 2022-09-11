c065bd8b20f021a4b64fee992954e2181de169d1
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-09-11 17:41:03 +0200

    fixed typo

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,7 +13,7 @@ fi
 .venv/bin/pip -q install -U pip
 .venv/bin/pip -q install -r requirements.txt
 
-if [[ ! -f /etc/systemd/system/jiwave-bot-service ]]
+if [[ ! -f /etc/systemd/system/jiwave-bot.service ]]
 then
   echo ""
   echo "WARN: Recommended way of running this bot (as service) seems not configured"
