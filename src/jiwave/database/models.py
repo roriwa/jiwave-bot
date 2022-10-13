08e97b149a6a25e5041b223f04ee48de20c1e45c
roriwa <robinrwalter2002@gmail.com>
2022-10-13 15:21:53 +0200

    setup of sqlalchemy

---
+++ src/jiwave/database/models.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+from sqlalchemy import Column, String, Integer, DateTime
+from .base import Base as BaseModel
+
+
+class TimerConfig(BaseModel):
+    __tablename__ = "timer-config"
+
+    guild_id = Column(Integer, nullable=False, primary_key=True)
+    channel_id = Column(Integer, nullable=False, primary_key=True, unique=True)
+    channel_orig_name = Column(String, nullable=False)
+    target_time = Column(DateTime, nullable=False)
+
+
+class ArchiveConfig(BaseModel):
+    __tablename__ = "archive-config"
+
+    guild_id = Column(Integer, nullable=False, primary_key=True)
+    source_id = Column(Integer, nullable=False, primary_key=True, unique=True)
+    target_id = Column(Integer, nullable=False)
+    emoticon = Column(String, nullable=False)
+
+
+class GuildConfig(BaseModel):
+    __tablename__ = "guild-config"
+
+    guild_id = Column(Integer, primary_key=True, unique=True)
+    message_template = Column(String, nullable=False)
+
+
+class LogRecord(BaseModel):
+    __tablename__ = "logs"
+
+    guild_id = Column(Integer, primary_key=True)
+    timestamp = Column(Integer, primary_key=True)
+    message = Column(String, nullable=False)
